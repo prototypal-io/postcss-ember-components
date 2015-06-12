200d3acdd790923a1e221945f1142eb0daa9f5bd
James Fickel <jamesfickel@gmail.com>
2015-06-11 20:34:48 -0700

    Determining the css origin file from css.source.input.from now

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var postcss = require('postcss');
+var path = require('path');
 
 module.exports = postcss.plugin('postcss-ember-components', function (opts) {
   opts = opts || {};
 
-  return function (css) {;
+  return function (css) {
     // var uuid = generateUUID();
     var dummyUUID = "abc123";
+    var fileName = path.basename(css.source.input.from, '.css');
+
     css.eachRule(function(rule) {
       // swap dummyUUID with uuid
       var selectorCollection = rule.selector.split(" ").map(function(selector) {
-        var result = "." + opts.fileName + "-" + dummyUUID,
+        var result = "." + fileName + "-" + dummyUUID,
             appendedSelector;
         if (selector !== ":--component" && rule.selector[0] === ".") {
           appendedSelector = selector.slice(1);
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var expect  = require('chai').expect;
 var plugin = require('../');
 
 var test = function (input, output, opts, done) {
-  postcss([ plugin(opts) ]).process(input).then(function (result) {
+  postcss([ plugin(opts) ]).process(input, { from: 'some/file/path/my-component.css' }).then(function (result) {
     expect(result.css).to.eql(output);
     expect(result.warnings()).to.be.empty;
     done();
@@ -17,12 +17,26 @@ describe('postcss-ember-components', function () {
   it('handles basic conversion with simple classes and :--component', function (done) {
     test(':--component { color: green; } .foo { color: red; } .bar { color: blue; }',
          '.my-component-abc123 { color: green; } .my-component-abc123-foo { color: red; } .my-component-abc123-bar { color: blue; }',
-         { fileName: 'my-component' }, done);
+         { }, done);
   });
 
   it('handles conversion with descendant classes', function (done) {
     test('.foo .bar { color: orange; }',
          '.my-component-abc123-foo .my-component-abc123-bar { color: orange; }',
-         { fileName: 'my-component' }, done);
+         { }, done);
   });
 });
+
+// var postcss = require('postcss');
+// var allComponentCSS;
+//
+// someLoopForComponentCSSFiles(function(fileName) {
+//   postcss([ require('postcss-ember-components')({ fileName: fileName }) ])
+//     .process(css, { from: fileName })
+//     .then(function (result) {
+//       allComponentCSS += result.css;
+//   });
+// });
+//
+// var lookupTableJSON = convertAllComponentCSSToJSON(allComponentCSS);
+// fs.writeFileSync('some.lookup.table', result.css);
